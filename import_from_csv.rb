bbac6fc013f6e254ae3774024a5163c41b9feda0
muricchristopher <murichristopher@outlook.com>
2022-07-15 22:02:13 -0300

    add ruby script to import csv file into db

---
+++ import_from_csv.rb
@@ -0,0 +1,18 @@
+require 'csv'
+require 'byebug'
+require './lib/medical_record'
+require './utils/CSVHandler.rb'
+
+def import_to_db(file)
+   csv = CSVHandler.read_file(file)
+
+   csv.each_with_index do | row, index|
+      MedicalRecord.new(row).create
+      print "Importing... [#{index + 1}/#{csv.length}]\n"
+   end
+
+   print "## CSV imported succesfully! ## \n"
+end
+
+import_to_db("data.csv")
+

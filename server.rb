5d3d9abe6fe1cbf415498d85905a94beec275625
muricchristopher <murichristopher@outlook.com>
2022-07-15 22:04:57 -0300

    get /tests return all medical records inserted in db

--- server.rb
+++ server.rb
@@ -1,40 +1,19 @@
 require 'sinatra'
 require 'rack/handler/puma'
 require 'csv'
-# set :public_folder, File.dirname(__FILE__) + "/static"
+require './lib/medical_record'
+require 'bundler'
+Bundler.require
 class Server < Sinatra::Base
   set :server, 'puma'
   set :bind, '0.0.0.0'
   set :port, 3000
 
-  get "/" do
-   "Hello World!"
-  end
-
   get '/tests' do
-    ### Example
-    # [
-    #   [cpf, name, date],
-    #   [123, 'Leandro', 2022-01-01'],
-    #   [456, 'Ana', 2022-01-02'],
-    #   [789, 'Maria', 2022-01-03']
-    # ]
-    #
-    rows = CSV.read("./data.csv", col_sep: ';')
+    @tests = MedicalRecord.all
 
-    ### Example
-    # [cpf, name, date]
-    columns = rows.shift
+    return "No medical record found".to_json if @tests.empty?
 
-    ### Example
-    # From:  [123, 'Leandro', 2022-01-01']
-    # To: { cpf: 123, name: 'Leandro', date: '2022-01-01' }
-    rows.map do |row|
-      row.each_with_object({}).with_index do |(cell, acc), idx|
-        column = columns[idx]
-        acc[column] = cell
-      end
-    end.to_json
+    jbuilder :tests
   end
-
 end

2273d1d9289c77ee2eceb54abc8e3c0363f4523a
muricchristopher <murichristopher@outlook.com>
2022-07-18 15:54:34 -0300

    add endpoint POST /import to import csv files directly into database

--- server.rb
+++ server.rb
@@ -2,7 +2,11 @@ require 'sinatra'
 require 'rack/handler/puma'
 require 'csv'
 require './lib/medical_record'
+require './utils/csv_handler'
 require 'bundler'
+require 'byebug'
+require 'pry'
+
 Bundler.require
 class Server < Sinatra::Base
   set :server, 'puma'
@@ -16,4 +20,19 @@ class Server < Sinatra::Base
 
     jbuilder :tests
   end
+
+  post "/import" do
+    file = params["csv_file"]["tempfile"]
+    csv = CSVHandler.read_file(file)
+
+    begin
+      csv.each do | row |
+        MedicalRecord.new(row).create
+      end
+      201
+    rescue
+      422
+    end
+  end
+
 end

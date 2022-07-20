cb0991da1de7bf4b601111d1fe70c962c3a1b3aa
muricchristopher <murichristopher@outlook.com>
2022-07-20 17:35:25 -0300

    test: create tests for get /tests/:token

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -27,6 +27,32 @@ describe Server, type: :request do
       end
    end
 
+   context "GET /tests/:id" do
+      it "should return test details from a token" do
+         first_data = {:cpf=>"048.973.170-88", :nome_paciente=>"Emilly Batista Neto", :email_paciente=>"gerald.crona@ebert-quigley.com", :data_nascimento_paciente=>"2001-03-11", :endereorua_paciente=>"165 Rua Rafaela", :cidade_paciente=>"Ituverava", :estado_patiente=>"Alagoas", :crm_mdico=>"B000BJ20J4", :crm_mdico_estado=>"PI", :nome_mdico=>"Maria Luiza Pires", :email_mdico=>"denna@wisozk.biz", :token_resultado_exame=>"IQCZ17", :data_exame=>"2021-08-05", :tipo_exame=>"hemácias", :limites_tipo_exame=>"45-52", :resultado_tipo_exame=>"97"}
+         second_data = {:cpf=>"048.973.170-88", :nome_paciente=>"Emilly Batista Neto", :email_paciente=>"gerald.crona@ebert-quigley.com", :data_nascimento_paciente=>"2001-03-11", :endereorua_paciente=>"165 Rua Rafaela", :cidade_paciente=>"Ituverava", :estado_patiente=>"Alagoas", :crm_mdico=>"B000BJ20J4", :crm_mdico_estado=>"PI", :nome_mdico=>"Maria Luiza Pires", :email_mdico=>"denna@wisozk.biz", :token_resultado_exame=>"IQCZ17", :data_exame=>"2021-08-05", :tipo_exame=>"glicemia", :limites_tipo_exame=>"25-83", :resultado_tipo_exame=>"78"}
+         expected_response = {"result_token"=>"IQCZ17", "result_date"=>"2021-08-05", "cpf"=>"048.973.170-88", "name"=>"Emilly Batista Neto", "email"=>"gerald.crona@ebert-quigley.com", "birthday"=>"2001-03-11", "doctor"=>{"crm"=>"B000BJ20J4", "crm_state"=>"PI", "name"=>"Maria Luiza Pires"}, "tests"=>[{"test_type"=>"hemácias", "test_limits"=>"45-52", "test_result"=>"97"}, {"test_type"=>"glicemia", "test_limits"=>"25-83", "test_result"=>"78"}]}
+         MedicalRecord.new(first_data).create
+         MedicalRecord.new(second_data).create
+
+         response = get "/tests/IQCZ17"
+         json_response = JSON.parse(response.body)
+
+         expect(response.content_type).to include('application/json')
+         expect(json_response).to eq(expected_response)
+         expect(response.status).to eq(200)
+      end
+
+      it "should return a status 404 and an error message when test details from a token were not found" do
+         response = get '/tests/asmdkasdjsandjandsadjsan'
+
+         json_response = JSON.parse(response.body)
+
+         expect(json_response["error"]).to eq("Could not find a medical record with token -> asmdkasdjsandjandsadjsan")
+         expect(response.status).to eq(404)
+      end
+   end
+
    context "POST /import_sync" do
       csv = Rack::Test::UploadedFile.new('./test_data.csv', 'csv')
 
